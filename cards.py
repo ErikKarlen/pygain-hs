73c0550770283849875c050612d10933f75bc2fd
Erik K <herr.morrare@gmail.com>
2019-11-09 21:00:43 +0100

    Fetch and load hearthstone data
    Add way to load card data over Internet
    and by loading local file

--- cards.py
+++ cards.py
@@ -1,4 +1,16 @@
-import hearthstone as hs
-import hearthstone_data as hsd
+from urllib.request import Request, urlopen
+import json
 
-print(hsd)
+# Fetch card data over Internet
+"""
+req = Request('http://api.hearthstonejson.com/v1/35747/enUS/cards.collectible.json',
+                      headers={'User-Agent': 'Mozilla/5.0'})
+source = urlopen(req).read()
+data = json.loads(source)
+"""
+
+# Use local file with card data
+json_file = open('cards.collectible.json', encoding='utf-8')
+data = json.load(json_file)
+
+print(data[0])
